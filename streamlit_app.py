20c94b146057890888317b8b84073365a555ca45
kiran <117361383+rajini218@users.noreply.github.com>
2022-11-04 14:21:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,7 @@ streamlit.text('🥗 Omega 3 & blueberry Oatmeal')
 streamlit.text(' 🐔 kale, spainach & rocket smoothie')
 streamlit.text(' 🥑🍞Hard-bolied free_range egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
